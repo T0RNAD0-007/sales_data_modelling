a56b6a6541d1eeb1ca45e1b04ccaf377a53ecd66
Shivam Bhongade <Shivam.Bhongade@Clarivate.com>
2024-06-14 14:36:08 +0530

    removed credentials

--- connection.py
+++ connection.py
@@ -2,9 +2,9 @@ import snowflake.connector as sc
 
 def create_conn():
     conn = sc.connect(
-        user = 'shivambhongade',
-        password = 'S#!v@m@1234',
-        account = 'keuzwie-bc34931',
+        user = 'xxx',
+        password = 'xxx',
+        account = 'xxx',
         database = 'RAW_SALES',
         warehouse = 'COMPUTE_WH',
         schema = 'PUBLIC'
